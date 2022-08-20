c17e0a7e088dc049acc5236952ea81190fccd5b8
Venantius Galih <galih@nawatech.co>
2022-08-20 15:28:27 +0700

    start over

--- COVID-19 US Tracking Sample/Model/queries/Table.m
+++
@@ -1,5 +0,0 @@
-let
-    Source = Table.FromRows(Json.Document(Binary.Decompress(Binary.FromText("i45Wcs7PS8ssyk1NUXBOLE4tVtJRMlSK1YlWcklNLMkAcY3AXJCkgltiSWJOZkmlQlBiSSpQylgpNhYA", BinaryEncoding.Base64), Compression.Deflate)), let _t = ((type text) meta [Serialized.Text = true]) in type table [Metric = _t, Order = _t]),
-    #"Changed Type" = Table.TransformColumnTypes(Source,{{"Metric", type text}, {"Order", Int64.Type}})
-in
-    #"Changed Type"
